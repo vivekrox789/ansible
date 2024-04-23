55a4d1f459cd44fd1d7b35d4bf0d7285ebff794f
vivek <vivekbca1998@gmail.com>
2024-04-23 17:44:23 +0000

    added 1

---
+++ apache2_script.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo "hello world from apache2 by Prashant" > /var/www/html/index.html
---
+++ nginx_script.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo "hello world from nginx by Prashant" > /var/www/html/index.nginx-debian.html
